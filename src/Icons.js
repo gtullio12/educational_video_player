9a638ff41dfcd06f08a9fd18e12c9779dae1c5c0
gtullio12 <gtullio12@protonmail.com>
2024-06-26 12:46:48 -1000

    Remove background image

--- src/Icons.js
+++ src/Icons.js
@@ -1,5 +1,4 @@
 import React from "react";
-import background from "./logos/background.png";
 
 export class ProfileIcon extends React.Component {
   render() {
@@ -31,19 +30,6 @@ export class CommentIcon extends React.Component {
       >
         <path d="M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9 9 0 0 0 8 15" />
       </svg>
-    );
-  }
-}
-
-export class BackgroundIcon extends React.Component {
-  render() {
-    return (
-      <img
-        width={400}
-        height={200}
-        src={background}
-        className="d-inline-block align-top"
-      />
     );
   }
 }
